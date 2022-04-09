17d589de5292eece8d9accb90f48a1fe3cf0bdda
mensur056 <mansur.sarxanov@gmail.com>
2022-04-09 12:44:35 +0400

    'test'

---
+++ lib/features/home/cubit/home_state.dart
@@ -0,0 +1,11 @@
+part of 'home_cubit.dart';
+
+
+
+
+abstract class HomeState extends Equatable{
+  const HomeState();
+  @override
+  List<Object>get props=>[];
+}
+class HomeInitial extends HomeState{}
