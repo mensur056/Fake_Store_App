5db61a6b88a55e3c2b51c7f7d4663a5b29916850
mensur056 <mansur.sarxanov@gmail.com>
2022-04-10 11:27:41 +0400

    'test'

--- lib/features/home/model/product_model.g.dart
+++ lib/features/home/model/product_model.g.dart
@@ -1,6 +1,12 @@
 part of 'product_model.dart';
 
-ProductModel _$ProductModelFromJson(Map<String, dynamic>json) =>
-    ProductModel()
-      ..id = json['id'] as int?
-    ..title=json['title']as
+ProductModel _$ProductModelFromJson(Map<String, dynamic> json) => ProductModel()
+  ..id = json['id'] as int?
+  ..title = json['title'] as String?
+  ..price = (json['price'] as num?)?.toDouble()
+  ..description = json['description'] as String?
+  ..category = json['category'] as String?
+  ..image = json['image'] as String?
+  ..rating = json['rating'] == null
+      ? null
+      : Rating.fromJson(json['rating'] as Map<String, dynamic>);
