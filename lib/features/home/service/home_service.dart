6722a826610d13125a505255a7a5bdaf1521cc69
mensur056 <mansur.sarxanov@gmail.com>
2022-04-10 11:55:25 +0400

    'test'

--- lib/features/home/service/home_service.dart
+++ lib/features/home/service/home_service.dart
@@ -1,8 +1,12 @@
 import 'package:vexana/vexana.dart';
 
+import '../model/product_model.dart';
+
 abstract class IHomeService {
   late final INetworkManager _networkManager;
 
   IHomeService(INetworkManager networkManager)
       : _networkManager = networkManager;
+
+  Future<List<ProductModel>?> fetchAllProducts({int count = 5});
 }
