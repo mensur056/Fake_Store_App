912f03d3b430e116160a61c8c7c37284e5158fc6
mensur056 <mansur.sarxanov@gmail.com>
2022-04-10 11:38:55 +0400

    'test'

--- lib/features/home/model/product_model.dart
+++ lib/features/home/model/product_model.dart
@@ -20,48 +20,10 @@ class ProductModel extends INetworkModel<ProductModel> {
 
   @override
   Map<String, dynamic> toJson() {
-    return _ProductModelToJson(this);
+    return _$ProductModelToJson(this);
   }
 }
 
-// class products {
-
-//
-//   products(
-//       {this.id,
-//         this.title,
-//         this.price,
-//         this.description,
-//         this.category,
-//         this.image,
-//         this.rating});
-//
-//   products.fromJson(Map<String, dynamic> json) {
-//     id = json['id'];
-//     title = json['title'];
-//     price = json['price'];
-//     description = json['description'];
-//     category = json['category'];
-//     image = json['image'];
-//     rating =
-//     json['rating'] != null ? new Rating.fromJson(json['rating']) : null;
-//   }
-//
-//   Map<String, dynamic> toJson() {
-//     final Map<String, dynamic> data = new Map<String, dynamic>();
-//     data['id'] = this.id;
-//     data['title'] = this.title;
-//     data['price'] = this.price;
-//     data['description'] = this.description;
-//     data['category'] = this.category;
-//     data['image'] = this.image;
-//     if (this.rating != null) {
-//       data['rating'] = this.rating!.toJson();
-//     }
-//     return data;
-//   }
-// }
-//
 @JsonSerializable()
 class Rating {
   final double? rate;
--- lib/features/home/model/product_model.g.dart
+++ lib/features/home/model/product_model.g.dart
@@ -10,3 +10,23 @@ ProductModel _$ProductModelFromJson(Map<String, dynamic> json) => ProductModel()
   ..rating = json['rating'] == null
       ? null
       : Rating.fromJson(json['rating'] as Map<String, dynamic>);
+
+Map<String, dynamic> _$ProductModelToJson(ProductModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'price': instance.price,
+      'description': instance.description,
+      'category': instance.category,
+      'image': instance.image,
+      'rating': instance.rating,
+    };
+
+Rating _$RatingFromJson(Map<String, dynamic> json) => Rating(
+      rate: (json['rate'] as num?)?.toDouble(),
+      count: json['count'] as int?,
+    );
+Map<String, dynamic>  _$RatingToJson(Rating instance)=><String ,dynamic>{
+  'rate':instance.rate,
+  'count':instance.count
+};
