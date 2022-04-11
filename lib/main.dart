1c4f73f23352114f5881094b68be01091175c3a4
mensur056 <mansur.sarxanov@gmail.com>
2022-04-11 10:15:28 +0400

    'test'

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.fallback(),
+      theme: ThemeData.dark(),
       home: const HomeView(),
     );
   }
