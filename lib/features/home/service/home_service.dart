c5f5240940a49055b05aacca9ce13c0825256ae5
mensur056 <mansur.sarxanov@gmail.com>
2022-04-11 09:26:00 +0400

    'test'

--- lib/features/home/service/home_service.dart
+++ lib/features/home/service/home_service.dart
@@ -2,6 +2,8 @@ import 'package:vexana/vexana.dart';
 
 import '../model/product_model.dart';
 
+enum _HomeServicePath { products }
+
 abstract class IHomeService {
   late final INetworkManager _networkManager;
 
@@ -15,7 +17,11 @@ class HomeService extends IHomeService {
   HomeService(INetworkManager networkManager) : super(networkManager);
 
   @override
-  Future<List<ProductModel>?> fetchAllProducts({int count = 5}) {
-    throw UnimplementedError();
+  Future<List<ProductModel>?> fetchAllProducts({int count = 5})async {
+    final response = await _networkManager.send<ProductModel, List<ProductModel>>(
+        _HomeServicePath.products.name,
+        parseModel: ProductModel(),
+        method: RequestType.GET);
+    return response.data;
   }
 }
